813838459c0648873d0b5f8e5a27873a3009db0a
ridhi-priya <ridhi7681309@gmail.com>
2025-01-19 23:50:38 +0530

    updated day04-palindromecheck

---
+++ js code/Day-04-PalindromeCheck.js
@@ -0,0 +1,15 @@
+var isPalindrome = function (x) {
+  if (x < 0) return false;
+  let xstr = x.toString().trim();
+  let rev = xstr.split("").reverse().join("");
+
+  if (xstr === rev) {
+    console.log(true);
+    return true;
+  } else {
+    console.log(false);
+    return false;
+  }
+};
+
+isPalindrome(121);
