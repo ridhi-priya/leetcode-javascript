ae2e2c20f3d422edb437cc55b1f52a8de4a7a3fd
ridhi-priya <ridhi7681309@gmail.com>
2025-01-21 23:56:52 +0530

    valid anagram method2 using object

--- js code/Day-06-vaild-anagram.js
+++ js code/Day-06-vaild-anagram.js
@@ -7,12 +7,31 @@
  * @param {string} t
  * @return {boolean}
  */
+
+// Solution 1
+// var isAnagram = function (s, t) {
+//   let s1 = s.split("").sort().join("");
+//   let t1 = t.split("").sort().join("");
+//   const ana = s1 === t1;
+//   console.log(ana);
+//   return ana;
+// };
+// isAnagram("anagram", "nagaram"); //true
+// isAnagram("cat", "car"); //true
+
+// solution2
 var isAnagram = function (s, t) {
-  let s1 = s.split("").sort().join("");
-  let t1 = t.split("").sort().join("");
-  const ana = s1 === t1;
-  console.log(ana);
-  return ana;
+  if (s.length !== t.length) return false;
+  let objs = {};
+  let objt = {};
+  for (let i = 0; i < s.length; i++) {
+    objs[s[i]] = (objs[s[i]] || 0) + 1;
+    objt[t[i]] = (objt[t[i]] || 0) + 1;
+  }
+  for (let key in objs) {
+    if (objs[key] !== objt[key]) return false;
+  }
+  return true;
 };
-isAnagram("anagram", "nagaram"); //true
-isAnagram("cat", "car"); //true
+console.log(isAnagram("anagram", "nagaram")); //true
+console.log(isAnagram("cat", "car")); //false
