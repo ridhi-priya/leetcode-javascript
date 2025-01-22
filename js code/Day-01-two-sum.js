526d828ab262f5e30c2bfcb6ac6aa269da368b7b
ridhi-priya <ridhi7681309@gmail.com>
2025-01-22 23:03:30 +0530

    added sol2 for two-sum

--- js code/Day-01-two-sum.js
+++ js code/Day-01-two-sum.js
@@ -3,17 +3,27 @@
 //  * Difficulty: Easy
 
 let nums = [2, 7, 11, 15];
-target = 9;
+target = 13;
 
 var twoSum = function (nums, target) {
-  const map = new Map();
   for (let i = 0; i < nums.length; i++) {
-    const finddiff = target - nums[i];
-    if (map.has(finddiff)) {
-      return [map.get(finddiff), i];
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
     }
-    map.set(nums[i], i);
+    return [];
   }
-  return [];
+
+  // var twoSum = function (nums, target) {
+  //   const map = new Map();
+  //   for (let i = 0; i < nums.length; i++) {
+  //     const finddiff = target - nums[i];
+  //     if (map.has(finddiff)) {
+  //       return [map.get(finddiff), i];
+  //     }
+  //     map.set(nums[i], i);
+  //   }
+  //   return [];
 };
 document.write(twoSum(nums, target));
---
+++ js code/Day-07-majority-element.js
@@ -0,0 +1,26 @@
+// 169. Majority Element
+// https://leetcode.com/problems/majority-element/description/?envType=problem-list-v2&envId=array
+//  * Difficulty: easy
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function (nums) {
+    let c=0
+    for(let i=0;i<nums.length;i++){
+        if()
+    }
+      
+  };
+// var majorityElement = function (nums) {
+//   if (nums.length === 1) return nums[0];
+//   let obj = {};
+//   for (let i = 0; i < nums.length; i++) {
+//     obj[nums[i]] = obj[nums[i]] + 1 || 1;
+//   }
+//   for(let key in obj){
+
+// };
+
+majorityElement([2, 2, 1, 1, 1, 2, 2]);
