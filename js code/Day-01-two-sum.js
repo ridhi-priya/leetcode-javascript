7a857d089080e70e5f366357a52fe87bf338416a
ridhi-priya <ridhi7681309@gmail.com>
2025-01-24 00:00:22 +0530

    added 3rd sol for two-sum

--- js code/Day-01-two-sum.js
+++ js code/Day-01-two-sum.js
@@ -4,17 +4,22 @@
 
 let nums = [2, 7, 11, 15];
 target = 13;
-
+// sol1
+// var twoSum = function (nums, target) {
+//   for (let i = 0; i < nums.length; i++) {
+//     for (let j = i + 1; j < nums.length; j++) {
+//       if (nums[i] + nums[j] === target) {
+//         return [i, j];
+//       }
+//     }
+//     return [];
+//   }
+// sol2
 var twoSum = function (nums, target) {
-  for (let i = 0; i < nums.length; i++) {
-    for (let j = i + 1; j < nums.length; j++) {
-      if (nums[i] + nums[j] === target) {
-        return [i, j];
-      }
-    }
-    return [];
+  
   }
 
+  // sol3
   // var twoSum = function (nums, target) {
   //   const map = new Map();
   //   for (let i = 0; i < nums.length; i++) {
