29141d3d80e38f8c27881ce646b1894b1d0b4f96
ridhi-priya <ridhi7681309@gmail.com>
2025-01-24 23:54:04 +0530

    added 2nd sol of best till to buy and sell stocks

--- js code/Day-08-best-time-to-buy-and-sell-stock.js
+++ js code/Day-08-best-time-to-buy-and-sell-stock.js
@@ -11,17 +11,32 @@ Output: -0;
  * @param {number[]} prices
  * @return {number}
  */
+
+//sol 1- using brute force approach time limit exceeded
+// var maxProfit = function (prices) {
+//   let globalProfit = 0;
+//   for (let i = 0; i < prices.length - 1; i++) {
+//     for (let j = i + 1; j < prices.length; j++) {
+//       const currProfit = Math.max(globalProfit, prices[j] - prices[i]);
+//       if (currProfit > globalProfit) {
+//         globalProfit = currProfit;
+//       }
+//     }
+//   }
+//   return globalProfit;
+// };
+
+//sol 2- using greedy approach
 var maxProfit = function (prices) {
-  let globalProfit = 0;
-  for (let i = 0; i < prices.length - 1; i++) {
-    for (let j = i + 1; i < prices.length; j++) {
-      const currProfit = Math.max(globalProfit, prices[j] - prices[i]);
-      if (currProfit > globalProfit) {
-        globalProfit = currProfit;
-      }
+  let min = prices[0] || 0;
+  let profit = 0;
+  for (let i = 0; i < prices.length; i++) {
+    if (min > prices[i]) {
+      min = prices[i];
     }
+    profit = Math.max(profit, prices[i] - min);
   }
-  return globalProfit;
+  return profit;
 };
 
 console.log(maxProfit([7, 1, 5, 3, 6, 4])); // Output: 5 (Buy at 1, Sell at 6)
