d070c83717a74bc917799d90cf12eafa7f72b3d6
ridhi-priya <ridhi7681309@gmail.com>
2025-01-30 23:02:02 +0530

    day-11-is-obj-empty

---
+++ js code/Day-11-is-obj-empty.js
@@ -0,0 +1,11 @@
+// 2703. Return Length of Arguments Passed
+// https://leetcode.com/problems/return-length-of-arguments-passed/description/?envType=study-plan-v2&envId=30-days-of-javascript
+//  * Difficulty: easy
+/**
+ * @param {Object|Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function (obj) {
+  if (Object.keys(obj).length === 0) return true;
+  return false;
+};
